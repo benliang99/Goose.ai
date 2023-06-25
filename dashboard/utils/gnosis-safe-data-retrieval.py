1879c76c3a74891a3b834892b4a88dacd9f4efa8
Benjamin S Liang <caliangben@gmail.com>
2023-06-24 23:56:52 -0400

    Added Gnosis descending order of value query for transactions

--- dashboard/utils/gnosis-safe-data-retrieval.py
+++ dashboard/utils/gnosis-safe-data-retrieval.py
@@ -7,16 +7,105 @@ import pandas as pd
 from datetime import datetime
 
 # Enter this URL into your browser and click "Explorer" to filter by data types
+# @andrew you can use this to generate queries to copy paste into these function 
+# templates. Example: you can use "where: {variable}"
 API_URL = 'https://api.thegraph.com/subgraphs/name/gjeanmart/gnosis-safe-goerli'
 
 def prepareJSON(query):
     r = requests.post(API_URL, json={'query': query})
     return json.loads(r.text)
 
-# Example query that retrieves 
-def walletInfo(address):
-    return
+# Query that retrieves all transactions with a Gnosis Safe wallet and 
+# sorts by descending order.
+def txDescending():
+    query = """
+    query {
+        transactions(where: {value_not: null}, orderBy: value, orderDirection: desc) {
+            block
+            data
+            destination
+            estimatedBaseGas
+            estimatedSafeTxGas
+            gasPrice
+            gasToken
+            id
+            hash
+            nonce
+            operation
+            payment
+            refundReceiver
+            signatures
+            stamp
+            status
+            txhash
+            value
+        }
+    }
+    """
+    json_data = prepareJSON(query)
+    transactions_data = json_data['data']['transactions']
+    transactions_df = pd.DataFrame(transactions_data)
+    transactions_df['stamp'] = pd.to_datetime(pd.to_numeric(transactions_df['stamp']), unit='s')
+    return transactions_df
 
+# Query that retrieves about any specific transaction
+def txInfo(address):
+    query = f"""
+    query {{
+        transaction(id: "{address}") {{
+            id
+            nonce
+            operation
+            payment
+            refundReceiver
+            signatures
+            stamp
+            status
+            txhash
+            value
+            hash
+            gasToken
+            gasPrice
+            estimatedBaseGas
+            estimatedSafeTxGas
+            destination
+            data
+            block
+        }}
+    }}
+    """
+    json_data = prepareJSON(query)
+    tx_data = json_data['data']['transaction']
+    tx_df = pd.DataFrame(tx_data, index=[0])
+    tx_df['stamp'] = pd.to_datetime(pd.to_numeric(tx_df['stamp']), unit='s')
+    return tx_df
+
+# Query that retrieves info about any specific wallet
+def walletInfo(address):
+    query = f"""
+    query {{
+        wallet(id: "{address}") {{
+            creator
+            currentNonce
+            factory
+            hash
+            id
+            mastercopy
+            network
+            owners
+            stamp
+            threshold
+        }}
+    }}
+    """
+    json_data = prepareJSON(query)
+    wallet_data = json_data['data']['wallet']
+    wallet_df = pd.DataFrame(wallet_data)
+    wallet_df['stamp'] = pd.to_datetime(pd.to_numeric(wallet_df['stamp']), unit='s')
+    return wallet_df
+
+# Returns the first wallets from the query, which is the same order every time, but 
+# the timestamps seem sort of random? Might be how the initial data upload worked
 def firstWalletsFromQuery(num):
     query = """
     query {
@@ -34,7 +123,7 @@ def firstWalletsFromQuery(num):
     wallets_df['stamp'] = pd.to_datetime(pd.to_numeric(wallets_df['stamp']), unit='s')
     return wallets_df
 
-def firstTransactionsFromQuery(num):
+def firstTxsFromQuery(num):
     query = """
     query {
         transactions(first: %d) {
@@ -50,8 +139,10 @@ def firstTransactionsFromQuery(num):
     transactions_df['stamp'] = pd.to_datetime(pd.to_numeric(transactions_df['stamp']), unit='s')
     return transactions_df
 
-# def main():
-#     print(firstWalletsFromQuery(10))
-#     return
+def main():
+    #print(walletInfo("0x00051af42c82bfa5c10e7ea75b71df8b58b10b44"))
+    #print(txInfo("0x0000ca5f43b6eaf5a05abe152f878f252df8d7178ed2ee617e2d58d59ffe49a5"))
+    print(txDescending())
+    return
 
-# main()
+main()
